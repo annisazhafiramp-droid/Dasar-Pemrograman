3b778506e35a291b486e02d8c87b75ccd2b07597
annisazhafiramp-droid <annisazhafiramp@gmail.com>
2026-08-28 08:08:10 +0700

    upload tugas1

---
+++ Dasar pemrograman/Project/MyFistJava04/Latihan04.java
@@ -0,0 +1,8 @@
+ public class Latihan04 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Zhafira");
+        System.out.println("Belajar Java itu mudah!");
+        System.out.println("Semakin sering mencoba, semakin jago");
+
+    }
+}
